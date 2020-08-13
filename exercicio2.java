b00f5db775cf75fd6b9fbf8cfdd5b46cd63246f7
Melissa <melissalongatti182@gmail.com>
2020-08-12 22:13:58 -0300

    exercicio 2 12/08

---
+++ exercicio2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class exercicio2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite algo? ");
+        String palavra = scanner.nextLine();
+
+        System.out.println("Digite algo? ");
+        float preco = scanner.nextFloat();
+
+        System.out.println(preco);
+
+    }
+}
+#src
