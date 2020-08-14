204392ceadf6b5aceb73e79d3509268332615c3d
Melissa <melissalongatti182@gmail.com>
2020-08-13 22:08:18 -0300

    aula 13/08

---
+++ aula7.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ //  || isso significa OU (or)
+public class aula7 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Digite uma senha?");
+        Scanner scanner = new Scanner(System.in);
+
+        int senha = scanner.nextInt();
+    }
+}
